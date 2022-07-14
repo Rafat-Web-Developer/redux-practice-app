423519e1dff1c7565b7fb4540b32ce0ce8c59a04
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-07-14 11:44:22 +0600

    Use Count State using useSelector

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Counter = () => {
+  const count = useSelector((state) => state.count);
+
   return (
     <div>
       <h1>Counter App</h1>
-      <h3>Count :</h3>
+      <h3>Count : {count}</h3>
       <button type="button">Increment</button>
       <button type="button">Decrement</button>
       <button type="button">Reset</button>
