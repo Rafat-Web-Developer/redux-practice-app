ef3bcab5479d7bc630474422af07d248ada35330
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-07-14 11:48:35 +0600

    Dispatch counter actions

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,16 +1,40 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrementAction,
+  incrementAction,
+  resetAction,
+} from "../../Redux/Actions/CounterAction";
 
 const Counter = () => {
   const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(incrementAction());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrementAction());
+  };
+
+  const handleReset = () => {
+    dispatch(resetAction());
+  };
 
   return (
     <div>
       <h1>Counter App</h1>
       <h3>Count : {count}</h3>
-      <button type="button">Increment</button>
-      <button type="button">Decrement</button>
-      <button type="button">Reset</button>
+      <button type="button" onClick={handleIncrement}>
+        Increment
+      </button>
+      <button type="button" onClick={handleDecrement}>
+        Decrement
+      </button>
+      <button type="button" onClick={handleReset}>
+        Reset
+      </button>
     </div>
   );
 };
