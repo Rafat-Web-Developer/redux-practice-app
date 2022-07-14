96f1cb75ea6212e5b529d5457d1bbd209977322c
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-07-14 11:26:19 +0600

    Counter app creating

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h3>Count :</h3>
+      <button type="button">Increment</button>
+      <button type="button">Decrement</button>
+      <button type="button">Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
